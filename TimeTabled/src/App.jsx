9cb2b6c8ae8bf283522969fddc76bb50a93deef2
Maliha <hossainmaliha18@gmail.com>
2025-02-24 16:31:45 -0800

    COmpleted calendar app

---
+++ TimeTabled/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Calendar from './components/Calendar'
+
+const App = () => {
+  return (
+    <div className="App">
+    <h1>Itinerary for 7 Days in Chicaco</h1> 
+    <h2>Welcome to Chicago, Natalia! Check out this calendar to get to know the city and see all the sites during your stay
+    <
